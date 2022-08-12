c44946f32b27bf34ba1a69c1b6e5383dc70a8617
陶鸿杰 <18790315365@163.com>
2022-08-12 22:29:45 +0800

    feat: 修改rollup配置文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "@rollup/plugin-dts";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
@@ -9,10 +9,18 @@ export default [
   {
     input: "src/index.ts",
     output: [
-      { file: packageJson.main, format: "cjs", sourcemap: true },
-      { file: packageJson.module, format: "esm", sourcemap: true },
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
     ],
-    plugin: [
+    plugins: [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
@@ -21,6 +29,6 @@ export default [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugin: [dts()],
+    plugins: [dts()],
   },
 ];
